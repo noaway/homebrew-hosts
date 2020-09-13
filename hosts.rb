e79b3ffe011c6207c22110dd75565ec99a56ecb2
wangyang <wangyang@sensorsdata.cn>
2020-09-13 22:13:36 +0800

    new version

--- hosts.rb
+++ hosts.rb
@@ -4,9 +4,9 @@
 class Hosts < Formula
   desc "hosts tool"
   homepage "https://github.com/noaway/hosts"
-  url "https://github.com/noaway/hosts/releases/download/0.1.3/hosts-mac64-0.1.3.tar.xz"
-  sha256 "4e503d8685c0dc9a5a89f651a800e7c296f9dae01ba899f18667ae39d6b853df"
-  version "0.1.3"
+  url "https://github.com/noaway/hosts/releases/download/v0.1.6/hosts-mac64-v0.1.6.tar.xz"
+  sha256 "b32814a9ae2c6c2a979fcb6388e90be4da738d5f6fc7c235b4e4931943845d96"
+  version "0.1.6"
   license "Apache-2.0"
   
   def install
